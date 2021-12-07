7544170c2aa332dbce72d1eee586994563582f9d
alexanyernas <alexanyernaranjo@gmail.com>
2021-12-07 08:19:36 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'memories-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
