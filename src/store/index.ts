4d71a3c001a3f46b3935c37352c452bd91272eb6
alexanyernas <alexanyernaranjo@gmail.com>
2021-12-09 11:46:45 -0400

    Details Page (Card)

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,7 @@ import { v4 } from 'uuid'
 
 const store = createStore({
     state: {
-        // memoryFind: {
-        //     id: '',
-        //     title: '',
-        //     description: ''
-        // } as Memory,
-        memories: [] as Memory[],
+        memories: [] as Memory[]
     },
     mutations: {
         addMemory ( state, payload: Memory ) {
@@ -17,10 +12,7 @@ const store = createStore({
         },
         deleteMemory ( state, payload: string ) {
             state.memories = state.memories.filter( memory => memory.id !== payload )
-        },
-        // memoryFilter ( state, payload: string ) {
-        //     state.memoryFind = state.memories.find( memory => memory.id === payload )
-        // } 
+        }
     },
     actions: {
         addMemory( {commit}, newMemory: Memory ) {
@@ -29,10 +21,12 @@ const store = createStore({
         },
         deleteMemory( {commit}, id: string ) {
             commit('deleteMemory', id)
-        },
-        // memoryFilter ( {commit}, id: string ) {
-        //     commit('memoryFilter', id)
-        // }
+        }
+    },
+    getters: {
+        getMemoryById: ( state ) => ( id: string ) => {
+            return state.memories.find( memory => memory.id === id )
+        }
     }
 })
 
--- src/views/DetailsPage.vue
+++ src/views/DetailsPage.vue
@@ -8,21 +8,33 @@
           >  
           </ion-back-button>
         </ion-buttons>
-        <ion-title>Hi!</ion-title>
+        <ion-title>{{ memoryFind.title }}</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-
+      <ion-card>
+        <ion-card-header>
+          <ion-card-subtitle>Alexanyer Naranjo</ion-card-subtitle>
+          <ion-card-title>{{ memoryFind.title }}</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          {{ memoryFind.description }}
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonBackButton } from '@ionic/vue';
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonBackButton, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader } from '@ionic/vue';
 
 export default  {
   name: 'DetailsPage',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonBackButton  }
-
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent },
+  data () {
+    return {
+      memoryFind: this.$store.getters.getMemoryById( this.$route.params.id )
+    }
+  }
 }
 </script>
